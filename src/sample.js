5af30f30ae81e83ce633329c8365a94d0b642ff0
keke <keke78ui9@outlook.com>
2023-04-12 23:20:52 -0700

    first pass javascript.

---
+++ src/sample.js
@@ -0,0 +1,16 @@
+function sum(a, b) {
+    return a + b;
+  }
+
+  function exponent(a,b) {
+    return a ** b;
+  }
+
+  function subtraction(a, b) {
+    return a -b;
+  }
+  module.exports = {
+    sum,
+    subtraction,
+    exponent
+  };
---
+++ test/sample.test.js
@@ -0,0 +1,13 @@
+const {exponent, subtraction, sum} = require('../src/sample');
+
+test('test sum', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('test subtraction', () => {
+    expect(subtraction(10, 2)).toBe(8);
+});
+
+test('test exponent', () => {
+    expect(exponent(5, 2)).toBe(25);
+});
